7d2fab75f0888c1d960df1ab10ab5c1996fc1284
Thomas C. Santhosh <tchakkaramakkils3886@student.pps.net>
2025-11-20 15:17:10 -0800

    si

---
+++ Wells-Robotics-Website/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  return(
+    <>
+    <head>
+      <title>Ida B. Wells Robotics</title>
+    </head>
+    <div>
+      <h1>Team #7776</h1>
+    </div>
+
+    </>
+  )
+}
+
+export default App
