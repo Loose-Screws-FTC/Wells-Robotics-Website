7bcac925b88573ce2d8a5659f94d2fa2e06b51c5
ThomasC109 <tchakkaramakkils3886@student.pps.net>
2025-11-21 19:29:48 -0800

    trying to deploy

--- Wells-Robotics-Website/src/App.jsx
+++ Wells-Robotics-Website/src/App.jsx
@@ -19,8 +19,8 @@ function App() {
       const elapsed = timestamp - startTimeReference.current;
       const start = elapsed/1000
 
-      setX(Math.cos(start)*200);
-      setY(Math.sin(start)*200);
+      setX(Math.cos(start)*50);
+      setY(Math.sin(start)*50);
 
       requestAnimationFrame(animate);
     }
@@ -30,27 +30,45 @@ function App() {
 
   return(
     <>
-      <div>
-        <h1>Team 7776</h1>
-      </div>
-      <div>
-        <img 
-        src={logo} 
-        alt = "TeamLogo" 
-        style={{
-          width:"200px",
-          height: "auto", 
-          position: "relative",
-          left: `${x+(viewportWidth/2)-100}px`, 
-          top: `${y+(viewportHeight/2)-(150)}px`,
-          transform: `rotateY(${((x-200)/200)*360}deg)`,
-          transformStyle: "preserve-3d",
+      <title>FTC 7776</title>
+      <header>
+        7776 Robotics
+      </header>
+        <div class = "top">
+          <img 
+            src={logo} 
+            alt = "TeamLogo" 
+            style={{
+            width:"200px",
+            height: "auto", 
+            // position: "relative",
+            // left: `${x+(viewportWidth/2)-100}px`, 
+            // top: `${y+(viewportHeight/2)-(150)}px`,
+            // transform: `rotateY(${((x-200)/200)*360}deg)`,
+            // transformStyle: "preserve-3d",
           }}/>
-      </div>
-      <div>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores, quia! Unde, alias minima! Perferendis, eaque. Esse consequuntur, adipisci facere rem dolorum voluptatum. Hic molestias, tenetur fugit rem error porro repudiandae.</p>
-      </div>
-      <p>{x}</p>
+        </div>
+      <section>
+        <div class="content">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores, quia! Unde, alias minima! Perferendis, eaque. Esse consequuntur, adipisci facere rem dolorum voluptatum. Hic molestias, tenetur fugit rem error porro repudiandae.</p>
+        </div>
+      </section>
+      <section>
+        <div class="content">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores, quia! Unde, alias minima! Perferendis, eaque. Esse consequuntur, adipisci facere rem dolorum voluptatum. Hic molestias, tenetur fugit rem error porro repudiandae.</p>
+        </div>
+      </section>
+      <section>
+        <div class="content">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores, quia! Unde, alias minima! Perferendis, eaque. Esse consequuntur, adipisci facere rem dolorum voluptatum. Hic molestias, tenetur fugit rem error porro repudiandae.</p>
+        </div>
+      </section>
+      <section>
+        <div class="content">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores, quia! Unde, alias minima! Perferendis, eaque. Esse consequuntur, adipisci facere rem dolorum voluptatum. Hic molestias, tenetur fugit rem error porro repudiandae.</p>
+        </div>
+      </section>
+ 
     </>
   )
 }
