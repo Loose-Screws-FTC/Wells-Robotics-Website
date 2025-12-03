3576cb731dadd3fdbdcde65200e8f2e940d0daa7
Thomas C. Santhosh <tchakkaramakkils3886@student.pps.net>
2025-12-03 15:10:17 -0800

    g

--- Wells-Robotics-Website/src/Pages/robot.jsx
+++ Wells-Robotics-Website/src/Pages/robot.jsx
@@ -22,7 +22,7 @@ export default function Robot(){
                 src="Wells-Robotics-Website/src/assets/Chassis Assembly.glb"
                 shadow-intensity="1"
                 camera-controls touch-action="pan-y"
-                style={{ width: "100%", height: "500px"}}
+                style={{ width: "100%", height: "50rem"}}
             ></model-viewer>
         </>
     )
